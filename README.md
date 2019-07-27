3e37ca4110fc32c15aac425ae2b2b6d971f9995c
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-07-27 10:13:30 +0200

    👌 IMPROVE: Just install the package no need to configure anything.

--- README.md
+++ README.md
@@ -2,10 +2,5 @@
 
 Vue support for Sublime's LSP plugin.
 
-1. Install [LSP](https://packagecontrol.io/packages/LSP), [Vue Syntax Highlight](https://packagecontrol.io/packages/Vue%20Syntax%20Highlight) and `LSP-vue` from Package Control.
-2. From the command palette run `LSP: Setup Vue server`.
-3. Restart Sublime.
-
-Server related issues report [here](https://github.com/vuejs/vetur).
-
-> Note: On Mac, if you have problems running `LSP: Setup Vue server`, install [Fix Mac Path](https://packagecontrol.io/packages/Fix%20Mac%20Path), and optionally set the `additional_path_items` in your user preferences.
+* Install [LSP](https://packagecontrol.io/packages/LSP), [Vue Syntax Highlight](https://packagecontrol.io/packages/Vue%20Syntax%20Highlight) and `LSP-vue` from Package Control.
+* Restart Sublime.
--- plugin.py
+++ plugin.py
@@ -1,126 +1,75 @@
 import shutil
-
+import os
 import sublime
-import sublime_plugin
+
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
-default_name = 'vue'
-server_package_name = 'vue-language-server'
 
-default_config = ClientConfig(
-    name=default_name,
-    binary_args=[
-        'vls'
-    ],
-    tcp_port=None,
-    enabled=True,
-    init_options=dict(),
-    settings={
-        "vetur": {
-            "validation": {
-                "template": True,
-                "style": True,
-                "script": True,
-            },
-            "completion": {
-                "autoImport": False,
-                "useScaffoldSnippets": False,
-            },
-            "format": {
-                "defaultFormatter": {
-                    "js": 'prettier',
-                    "ts": 'prettier',
-                },
-                "defaultFormatterOptions": {},
-                "scriptInitialIndent": False,
-                "styleInitialIndent": False,
-            },
-        },
-        "css": {},
-        "html": {
-            "suggest": {}
-        },
-        "javascript": {
-            "format": {}
-        },
-        "typescript": {
-            "format": {}
-        },
-        "emmet": {},
-        "stylusSupremacy": {},
-    },
-    env=dict(),
-    languages=[
-        LanguageConfig(
-            'vue',
-            ['text.html.vue'],
-            ["Packages/Vue Syntax Highlight/Vue Component.sublime-syntax"]
+package_path = os.path.dirname(__file__)
+server_path = os.path.join(package_path, 'node_modules', 'vue-language-server', 'bin', 'vls')
+
+
+def plugin_loaded():
+    print('LSP-vue: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
+
+    if not os.path.isdir(os.path.join(package_path, 'node_modules')):
+        # install server if no node_modules
+        print('LSP-vue: Installing server.')
+        sublime.active_window().run_command(
+            "exec", {
+                "cmd": [
+                    "npm",
+                    "install",
+                    "--verbose",
+                    "--prefix",
+                    package_path
+                ]
+            }
         )
-    ]
-)
-
-# Dependencies that needs to be installed for the server to work
-dependencies = ['node', 'vls']
+        sublime.message_dialog('LSP-vue\n\nRestart sublime after the server has been installed successfully.')
 
 
-def is_installed(dependency):
-    return shutil.which(dependency) is not None
-
-
-class LspVueSetupCommand(sublime_plugin.WindowCommand):
-    def is_visible(self):
-        if not is_installed('node') or not is_installed('vls'):
-            return True
-        return False
-
-    def run(self):
-        if not is_installed('node'):
-            sublime.message_dialog(
-                "Please install Node.js before running setup."
-            )
-            return
-
-        if not is_installed('vls'):
-            should_install = sublime.ok_cancel_dialog(
-                "vls was not in the PATH.\nInstall {} globally now?".format(
-                    server_package_name)
-            )
-            if should_install:
-                self.window.run_command(
-                    "exec", {
-                        "cmd": [
-                            "npm",
-                            "install",
-                            "--verbose",
-                            "-g",
-                            server_package_name
-                        ]
-                    })
-        else:
-            sublime.message_dialog(
-                "{} is already installed".format(server_package_name)
-            )
+def is_node_installed():
+    return shutil.which('node') is not None
 
 
 class LspVuePlugin(LanguageHandler):
-    def __init__(self):
-        self._name = default_name
-        self._config = default_config
-
     @property
     def name(self) -> str:
-        return self._name
+        return 'lsp-vue'
 
     @property
     def config(self) -> ClientConfig:
-        return self._config
+        settings = sublime.load_settings("LSP-vue.sublime-settings")
+        print('settingsssss')
+        print(settings.get('config'))              
+        return ClientConfig(
+            name='lsp-vue',
+            binary_args=[
+                'node',
+                server_path
+            ],
+            tcp_port=None,
+            enabled=True,
+            init_options={
+                "config": settings.get('config')
+            },
+            settings=dict(),
+            env=dict(),
+            languages=[
+                LanguageConfig(
+                    'vue',
+                    ['text.html.vue'],
+                    ["Packages/Vue Syntax Highlight/Vue Component.sublime-syntax"]
+                )
+            ]
+        )
 
     def on_start(self, window) -> bool:
-        for dependency in dependencies:
-            if not is_installed(dependency):
-                sublime.status_message('Run: LSP: Setup Vue server')
-                return False
+        if not is_node_installed():
+            sublime.status_message('Please install Node.js for the Vue Language Server to work.')
+            return False
         return True
 
     def on_initialized(self, client) -> None:
