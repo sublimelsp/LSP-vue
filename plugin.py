2d7a719d55a32e1d403eda38f63197a4cd29c2b5
Rafał Chłodnicki <rchl2k@gmail.com>
2020-03-22 21:29:34 +0100

    Don't allow session to start if server is not ready (#19)

--- plugin.py
+++ plugin.py
@@ -86,7 +86,7 @@ class LspVuePlugin(LanguageHandler):
         if not is_node_installed():
             sublime.status_message('Please install Node.js for the Vue Language Server to work.')
             return False
-        return True
+        return server.ready
 
     def on_initialized(self, client) -> None:
         pass   # extra initialization here.
