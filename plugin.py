75e7b49deb953d3ed288fc3bc775314af83771e5
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-26 20:25:52 +0100

    refactor: clean up types and linting

--- plugin.py
+++ plugin.py
@@ -1,22 +1,34 @@
 from __future__ import annotations
+
 from LSP.plugin import Notification
-from LSP.plugin.core.types import cast
-from LSP.plugin.core.typing import Any, Callable, List, Mapping, Required, Tuple, TypedDict, Union
-from LSP.plugin.core.protocol import Error, ExecuteCommandParams, LSPAny, Location
+from LSP.plugin.core.protocol import Error
 from LSP.plugin.locationpicker import LocationPicker
+from LSP.protocol import ExecuteCommandParams
+from LSP.protocol import Location
+from LSP.protocol import LSPAny
 from lsp_utils import notification_handler
 from lsp_utils import NpmClientHandler
 from pathlib import Path
+from typing import Any
+from typing import Callable
+from typing import cast
+from typing import final
+from typing import List
+from typing import Tuple
+from typing import TypedDict
+from typing import Union
+from typing_extensions import override
 import sublime
 
-PACKAGE_NAME = __package__
+PACKAGE_NAME = str(__package__)
 SERVER_DIRECTORY = 'server'
 SERVER_NODE_MODULES = Path(SERVER_DIRECTORY) / 'node_modules'
-SERVER_BINARY_PATH =  SERVER_NODE_MODULES / '@vue' / 'language-server' / 'bin' / 'vue-language-server.js'
+SERVER_BINARY_PATH = SERVER_NODE_MODULES / '@vue' / 'language-server' / 'bin' / 'vue-language-server.js'
 
 
 class TypescriptTsserverCommandParams(TypedDict):
-    file: Required[str]
+    file: str
+
 
 TsserverRequestParams = Tuple[Tuple[int, str, Union[TypescriptTsserverCommandParams, List[str]]]]
 
@@ -29,25 +41,28 @@ def plugin_unloaded():
     LspVuePlugin.cleanup()
 
 
+@final
 class LspVuePlugin(NpmClientHandler):
     package_name = PACKAGE_NAME
     server_directory = SERVER_DIRECTORY
-    server_binary_path = SERVER_BINARY_PATH
+    server_binary_path = str(SERVER_BINARY_PATH)
 
     @classmethod
+    @override
     def required_node_version(cls) -> str:
         return '>=18'
 
-    def on_pre_server_command(self, command: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
+    @override
+    def on_pre_server_command(self, command: ExecuteCommandParams, done_callback: Callable[[], None]) -> bool:
         command_name = command['command']
         if command_name == 'editor.action.showReferences':
-            _, __, references = command['arguments']
-            self._handle_show_references(references)
+            _, __, references = command.get('arguments', [None, None, None])
+            self._handle_show_references(cast(list[Location], references))
             done_callback()
             return True
         return False
 
-    def _handle_show_references(self, references: List[Location]) -> None:
+    def _handle_show_references(self, references: list[Location]) -> None:
         session = self.weaksession()
         if not session:
             return
@@ -55,12 +70,12 @@ class LspVuePlugin(NpmClientHandler):
         if not view:
             return
         if len(references) == 1:
-            args = {
-                'location': references[0],
-                'session_name': session.config.name,
-            }
             window = view.window()
             if window:
+                args: dict[str, Any] = {
+                    'location': references[0],
+                    'session_name': session.config.name,
+                }
                 window.run_command('lsp_open_location', args)
         elif references:
             LocationPicker(view, session, references, side_by_side=False)
@@ -87,12 +102,13 @@ class LspVuePlugin(NpmClientHandler):
             'command': 'typescript.tsserverRequest',
             'arguments': [
                 command_name,
-                cast(LSPAny, command_params),
-                { 'isAsync': True, 'lowPriority': True },
+                command_params,
+                {'isAsync': True, 'lowPriority': True},
             ]
         }
-        session.execute_command(execute_command_params, progress=False) \
-            .then(lambda result: self._on_execute_command_response(seq, result))
+        session.execute_command(execute_command_params, progress=False).then(
+            lambda result: self._on_execute_command_response(seq, result)
+        )
 
     def _on_execute_command_response(self, seq: int, result: LSPAny | Error) -> None:
         session = self.weaksession()
